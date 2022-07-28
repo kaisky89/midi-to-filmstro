4ed20f2dc96bb9b7e40f70a82e3188fd5fa31c44
Kai Wittmann <kaisky89@gmail.com>
2022-07-28 02:13:12 +0200

    ✨ introduce some extras

---
+++ src/extras.js
@@ -0,0 +1,15 @@
+export const duplicateSections = () => {
+  window.stroDaw.clickHandler3({}, document.querySelector('.daw-item-0 .daw-item-top'))
+  window.stroDaw.changeSectionsPlus(+100)
+}
+
+export const startAudition = () => {
+  const auditionCheckmarkElement = document.querySelector('#audition-music')
+  if (auditionCheckmarkElement.checked) return
+  auditionCheckmarkElement.checked = true
+  window.stroDaw.setPunchAll()
+}
+
+export const muteWatermark = () => {
+  window.stroPlayer.splayer.watermarkPlayer.mute = true
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import './console'
+import { duplicateSections, muteWatermark, startAudition } from './extras'
 import { MidiLearner } from './MidiLearner'
 import midiMapper from './midiMapper'
 
 const midiLearner = new MidiLearner(midiMapper)
 
-if (!midiMapper.hasSetupMap) midiLearner.learn()
+const run = async () => {
+  if (!midiMapper.hasSetupMap) await midiLearner.learn()
+
+  duplicateSections()
+  startAudition()
+  muteWatermark()
+}
+run()
